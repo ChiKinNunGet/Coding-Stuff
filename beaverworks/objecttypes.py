28ea868f10ec1b24d95ff0fa58983708f6547341
ChiKinNunGet <Jake434546@gmail.com>
2022-02-14 22:08:21 -0800

    pp

--- beaverworks/objecttypes.py
+++ beaverworks/objecttypes.py
@@ -39,9 +39,42 @@ def numbers():
         math.factorial(4)
     """
     """
-    integers are any whole number
-    """
+    Floating point numbers
+        floating point is any number with a decimal place, referred to as float and 
+        Floats an be expressed in scientific notation
+        138000 can be 1.38e-5
 
+        Float precision
+            changing a floating point outside its decimal place does nothing
+                1.0 + 1e-16 = 1.0
+            floating point numbers also result in strange behavior
+            1.0+1.0+1.0-3.0 !=0 most times
+        Never check if two floating point numbers are equal, check if they are close enough.
+        math.isclose((0.1+0.1+0.1-0.3), 0., abs_tol=1e-9)
+    """
+    """
+    Complex numbers
+        Standard format: a+bj
+            Use j because in engineering current is expressed as i
+        variable name: complex
+            2+3j
+            complex (2, 3)
+            complex (0, 1)**2
+            all ways, but only j does not work. 1j can work
+        Accessing different parts
+            x = complex(2+3j)
+            x.real = 2
+            x.imag = 3            
+    """
+    """
+    Assignment statements
+        incrementing numbers or replacing it with a number
+            x = x+1
+        can be replaced with
+            x+=1
+        or any other operation
+    """
+    
 """
 booleans
 null
