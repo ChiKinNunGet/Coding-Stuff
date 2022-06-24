7c6531f38038c2ef68038905173063e4f1c805dc
ChiKinNunGet <Jake434546@gmail.com>
2022-06-23 21:16:55 -0700

    okgamng

--- beaverworks/Python_Classwork/Python_Coding_Assignments/odds_and_ends.py
+++ beaverworks/Python_Classwork/Python_Coding_Assignments/odds_and_ends.py
@@ -2,4 +2,6 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-data = np.load("mystery-img.npy")
+data = np.load('/Users/jake/Documents/Coding-Stuff/beaverworks/Python_Classwork/Python_Coding_Assignments/mystery-img.npy')
+plt.imshow(data, cmap='gray')
+plt.show()
